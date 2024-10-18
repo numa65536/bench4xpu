11b57c0612ca954c2d941ebf08548ac1f3cf2169
Emmanuel QUEMENER <emmanuel.quemener@gmail.com>
2024-10-18 10:18:06 +0200

    Minor changes in codes.

--- ETSN/MyDFT_OpenACC.c
+++ ETSN/MyDFT_OpenACC.c
@@ -1,9 +1,10 @@
-/* Simple Discrete Fourier Transform implemented in C and OpenMP/C */
-/* compilation with : gcc -fopenmp -O3 -o MyDFT_OpenMP MyDFT_OpenMP.c -lm -lgomp */
+/* Simple Discrete Fourier Transform implemented in C and OpenACC/C */
+/* compilation with : gcc -O3 -fopenacc -foffload=nvptx-none -foffload="-O3 -misa=sm_35 -lm" -o MyDFT_OpenACC MyDFT_OpenACC.c -lm */
 
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <openacc.h>
 #include <sys/time.h>
 
 #define PI 3.141592653589793
@@ -25,20 +26,25 @@ void MyDFT(MYFLOAT *A, MYFLOAT *B, MYFLOAT *a, MYFLOAT *b,int size)
     }
 }
 
-void MyDFTOpenMP(MYFLOAT *A, MYFLOAT *B, MYFLOAT *a, MYFLOAT *b,int size)
+void MyDFTOpenACC(MYFLOAT *A, MYFLOAT *B, MYFLOAT *a, MYFLOAT *b,int size)
 {
-  #pragma omp parallel for
-  for (uint j=0;j<size;j++)
-    {
-      MYFLOAT At=0.,Bt=0.;
-      for (uint i=0; i<size;i++) 
+
+#pragma acc data copy(a[0:size],b[0:size],A[0:size],B[0:size])
+#pragma acc parallel loop 
+    for (uint j=0;j<size;j++)
+      {
+	MYFLOAT At=0.,Bt=0.;
+#pragma acc seq reduction(+: At) reduction(+: Bt)
 	{
-	  At+=a[i]*cos(2.*PI*(MYFLOAT)(j*i)/(MYFLOAT)size)+b[i]*sin(2.*PI*(MYFLOAT)(j*i)/(MYFLOAT)size);
-	  Bt+=-a[i]*sin(2.*PI*(MYFLOAT)(j*i)/(MYFLOAT)size)+b[i]*cos(2.*PI*(MYFLOAT)(j*i)/(MYFLOAT)size);
-	}
-      A[j]=At;
-      B[j]=Bt;
-    }
+	for (uint i=0; i<size;i++) 
+	  {
+	    At+=a[i]*cos(2.*PI*(MYFLOAT)(j*i)/(MYFLOAT)size)+b[i]*sin(2.*PI*(MYFLOAT)(j*i)/(MYFLOAT)size);
+	    Bt+=-a[i]*sin(2.*PI*(MYFLOAT)(j*i)/(MYFLOAT)size)+b[i]*cos(2.*PI*(MYFLOAT)(j*i)/(MYFLOAT)size);
+	  }
+	A[j]=At;
+	B[j]=Bt;
+      }
+      }
 }
 
 int main(int argc,char *argv[])
@@ -54,10 +60,10 @@ int main(int argc,char *argv[])
     printf("\n\tEstimate DFT\n\n\t\t#1 : size (default 1024)\n\n");
   }
 
-  a=(float*)malloc(size*sizeof(MYFLOAT));
-  b=(float*)malloc(size*sizeof(MYFLOAT));
-  A=(float*)malloc(size*sizeof(MYFLOAT));
-  B=(float*)malloc(size*sizeof(MYFLOAT));
+  a=(MYFLOAT*)malloc(size*sizeof(MYFLOAT));
+  b=(MYFLOAT*)malloc(size*sizeof(MYFLOAT));
+  A=(MYFLOAT*)malloc(size*sizeof(MYFLOAT));
+  B=(MYFLOAT*)malloc(size*sizeof(MYFLOAT));
 
   for (int i=0;i<size;i++)
     {
@@ -68,11 +74,11 @@ int main(int argc,char *argv[])
     }
 
   gettimeofday(&tv1, NULL);
-  MyDFTOpenMP(A,B,a,b,size);
+  MyDFTOpenACC(A,B,a,b,size);
   gettimeofday(&tv2, NULL);
 
-  MYFLOAT elapsedOMP=(MYFLOAT)((tv2.tv_sec-tv1.tv_sec) * 1000000L +
-			    (tv2.tv_usec-tv1.tv_usec))/1000000;
+  MYFLOAT elapsedOpenACC=(MYFLOAT)((tv2.tv_sec-tv1.tv_sec) * 1000000L +
+				   (tv2.tv_usec-tv1.tv_usec))/1000000;
 
   /* printf("A=["); */
   /* for (int i=0;i<size;i++) */
@@ -91,12 +97,12 @@ int main(int argc,char *argv[])
   printf("\nA[0]=%.3f A[%i]=%.3f\n",A[0],size-1,A[size-1]);
   printf("B[0]=%.3f B[%i]=%.3f\n\n",B[0],size-1,B[size-1]);
 
-  printf("OpenMPElapsed Time: %.3f\n",elapsedOpenMP);
-  printf("OpenMPRate: %.i\n",(int)((float)size/elapsedOpenMP));
+  printf("ElapsedOpenACC: %.3f\n",elapsedOpenACC);
+  printf("OpenACCRate: %.i\n",(int)((float)size/elapsedOpenACC));
   
   free(a);
   free(b);
   free(A);
   free(B);
 }
-  
+
